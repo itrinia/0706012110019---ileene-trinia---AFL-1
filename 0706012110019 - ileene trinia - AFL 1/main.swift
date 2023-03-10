759a6d4c6c27412fde9de5eac1793ad2efa7b6e9
itrinia <ileenesantoso@gmail.com>
2023-03-10 10:25:37 +0700

    gotri es cendol

--- 0706012110019 - ileene trinia - AFL 1/main.swift
+++ 0706012110019 - ileene trinia - AFL 1/main.swift
@@ -258,27 +258,27 @@ func tukutuku(){
 }
 
 func gotri(){
-    var inputtuku: String = ""
-    var jtahu: Int = 0
-    var jnaskun: Int = 0
-    var jnascam: Int = 0
-    var jair: Int = 0
-    var jlemper: Int = 0
-    var jpoci: Int = 0
+    var inputgotri: String = ""
+    var jescendol: Int = 0
+    var jlanggi: Int = 0
+    var jsuun: Int = 0
+    var jnasbak: Int = 0
+    var jrendang: Int = 0
+    var jtt: Int = 0
     
     while true{
-        print("\nWelcome to tuku-tuku 💰 \nwhat would you like to order?\n\n[1] tahu isi\n[2] nasi kuning\n[3] nasi campur\n[4] air mineral\n[5] lemper\n[6] teh poci\n-\n[B]ack to main menu\nYour menu choice?")
+        print("\nWelcome to tuku-tuku 💰 \nwhat would you like to order?\n\n[1] es cendol suji\n[2] nasi langgi\n[3] nasi campur suun\n[4] nasi bakar\n[5] nasi rendang\n[6] tahu telor\n-\n[B]ack to main menu\nYour menu choice?")
         
-        inputtuku = readLine()!
-        if inputtuku == "1"{
-            print("tahu isi @10k IDR \nhow many tahu isi do you want to buy?")
-                if let jumtahu = readLine(), let jumtahu = Int(jumtahu) {
-                    jtahu = jumtahu
-                    if jtahu > 0 {
+        inputgotri = readLine()!
+        if inputgotri == "1"{
+            print("es cendol suji @16.3k IDR \nhow many es cendol suji do you want to buy?")
+                if let jumescendol = readLine(), let jumescendol = Int(jumescendol) {
+                    jescendol = jumescendol
+                    if jescendol > 0 {
                         // Use the jumlah variable here
-                        let price = Double(jtahu) * 10000
-                            print("\n🛒Added \(jtahu) tahu isi to cart for \(price) IDR🛒 \n🛍️Thank you for ordering 🛍️\n\n")
-                            shoppingCart.append((cafetaria: "tuku-tuku", order: "tahu isi \(jtahu)x"))
+                        let price = Double(jescendol) * 16300
+                            print("\n🛒Added \(jescendol) es cendol suji to cart for \(price) IDR🛒 \n🛍️Thank you for ordering 🛍️\n\n")
+                            shoppingCart.append((cafetaria: "gotri", order: "es cendol suji \(jtahu)x"))
                             totalPrice += price
                         tukutuku()
                     } else {
@@ -291,7 +291,7 @@ func gotri(){
                 }
                 break
             
-        } else if inputtuku == "2"{
+        } else if inputgotri == "2"{
             print("nasi kuning @18k IDR \nhow many nasi kuning do you want to buy?")
                 if let jumnaskun = readLine(), let jumnaskun = Int(jumnaskun) {
                     jnaskun = jumnaskun
@@ -312,7 +312,7 @@ func gotri(){
                 }
                 break
             
-        }else if inputtuku == "3"{
+        }else if inputgotri == "3"{
             print("nasi campur @18k IDR \nhow many nasi kuning do you want to buy?")
                 if let jumnascam = readLine(), let jumnascam = Int(jumnascam) {
                     jnascam = jumnascam
@@ -333,7 +333,7 @@ func gotri(){
                 }
                 break
             
-        }else if inputtuku == "4"{
+        }else if inputgotri == "4"{
             print("air mineral @5k IDR \nhow many air mineral do you want to buy?")
                 if let jumair = readLine(), let jumair = Int(jumair) {
                     jair = jumair
@@ -354,7 +354,7 @@ func gotri(){
                 }
                 break
             
-        }else if inputtuku == "5"{
+        }else if inputgotri == "5"{
             print("lemper @8k IDR \nhow many lemper do you want to buy?")
                 if let jumlemper = readLine(), let jumlemper = Int(jumlemper) {
                     jlemper = jumlemper
@@ -375,7 +375,7 @@ func gotri(){
                 }
                 break
             
-        }else if inputtuku == "6"{
+        }else if inputgotri == "6"{
             print("teh poci @6k IDR \nhow many teh poci do you want to buy?")
                 if let jumpoci = readLine(), let jumpoci = Int(jumpoci) {
                     jpoci = jumpoci
@@ -397,7 +397,7 @@ func gotri(){
                 break
             
             //code
-        }else if inputtuku.caseInsensitiveCompare("b") == .orderedSame {
+        }else if inputgotri.caseInsensitiveCompare("b") == .orderedSame {
             print("back to main menu wushhhhh 🏃🏻\n")
             main()
         } else {
